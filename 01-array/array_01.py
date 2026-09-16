4e63a6b505d31d24d466452f98131c4700d95e7b
sayali-chougule <sayalichougule2269@gmail.com>
2026-09-16 15:22:45 -0400

    array

--- 01-array/array_01.py
+++ 01-array/array_01.py
@@ -8,7 +8,18 @@ val1 = array('u', ['s', 'a', 'y', 'a', 'l', 'i'])
 for i in val:
     print(i, end=",")
 
+print(val.typecode)
+
 print('\n')
 
 for x in val1:
     print(x, end="")
+print(f" -", val1.typecode)
+
+
+# Reversing the Array
+
+val.reverse()
+
+for i in val:
+    print(i, end=",")
