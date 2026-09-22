3be02de5d330ea4bd1a1772e0fdff9ce8efd2838
sayali-chougule <sayalichougule2269@gmail.com>
2026-09-22 11:48:44 -0400

    DoublyLL

--- 03-DoublyLL/DoublyLL.py
+++ 03-DoublyLL/DoublyLL.py
@@ -10,4 +10,16 @@ class DoublyLL:
         self.head = None
 
     def insertAtEnd(self, value):
+    # for creating 1st (new) node in LinkedList
         temp = Node(value)
+        if self.head == None:
+            self.head = temp
+            return
+    
+    # creating a node at end
+        t = self.head
+        while t.next != None:
+            t = t.next
+    
+    
+        
