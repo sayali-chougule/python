765bd4d09a3f10e26d4ba41b74aca454599030db
sayali-chougule <sayalichougule2269@gmail.com>
2026-09-25 00:29:41 -0400

    21-merge_two_sorted_lists

---
+++ 02-SinglyLL/02-21_merge_two_sorted_lists.py
@@ -0,0 +1,24 @@
+class ListNode:
+    def __init__(self,val=0, next=None):
+        self.val=val
+        self.next=next
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+
+    # l1 = [1,3,5]
+    # l2 = [1,4,6]
+
+        dummy = ListNode()
+        curr = dummy
+
+        while l1 is not None and l2 is not None:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+        curr.next = l1 or l2
+        return dummy.next
